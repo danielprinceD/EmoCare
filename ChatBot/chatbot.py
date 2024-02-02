323f12c7804f44a353ef053eb775dfae6dcdac62
daniel <d.danielprince2003@gmail.com>
2024-02-02 19:48:07 +0530

    feat:Speech Recognition and response on Bot

--- ChatBot/chatbot.py
+++ ChatBot/chatbot.py
@@ -1,4 +1,6 @@
 import speech_recognition as sp
+from gtts import gTTS
+import os
 class ChatBot():
     def __init__(self,name):
         print("------Start---------")
@@ -12,9 +14,22 @@ class ChatBot():
             self.text = reg.recognize_google(audio)
             print("me-->",self.text)
         except:
-            print("me-->ERROR")            
+            print("me-->ERROR")   
+    def wake(self,text):
+        return True if self.name in text else False  
+    
+    @staticmethod
+    def text_to_speech(text):
+        print("--AI---",text)
+        speaker = gTTS(text=text,lang="en",slow=False)
+        speaker.save("response.mp3")
+        os.system("start response.mp3")
         
 if __name__ == "__main__":
-    ai = ChatBot(name = "comp")
+    ai = ChatBot(name = "bot")
     ai.speech_to_text()
-
+    if ai.wake(ai.text) is True:
+          res = "Hello I'm Bot,What are you Doing"
+    else :
+        res = "That is not my name."
+    ChatBot.text_to_speech(res)
